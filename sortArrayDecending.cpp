1eb1cf4c38b0f32f97f122f2cfa6c1783a39038b
MR Bips <113910627+rabiulbipul@users.noreply.github.com>
2022-11-19 12:38:46 +0600

    Create sortArrayDecending.cpp

---
+++ sortArrayDecending.cpp
@@ -0,0 +1,13 @@
+for(i=0;i<s3; i++)
+        {
+           for(k=0;k<s3-1;k++)
+             {
+         
+                if(arr3[k]<=arr3[k+1])
+                 {
+                   j=arr3[k+1];
+                   arr3[k+1]=arr3[k];
+                   arr3[k]=j;
+                 }  
+              }
+         }             
